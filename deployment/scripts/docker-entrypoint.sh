94e9a3a66e0a99da5e10fee9939be500ffa96571
Debbykalu <debbykalu@gmail.com>
2026-07-18 13:23:22 -0700

    Add deployment files

---
+++ deployment/scripts/docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+echo "Running database migrations..."
+
+flask db upgrade
+
+echo "Starting Gunicorn..."
+
+exec gunicorn \
+    --bind 0.0.0.0:${PORT:-8000} \
+    --workers 2 \
+    --threads 4 \
+    starter:app
